0e0feb4b8e359477afbe67cbf1aa244854045f84
Jose marcos <72453138+SakakibaraKun@users.noreply.github.com>
2021-11-05 18:13:30 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/jose-marcos-efigenio-b166b21a6/" target="_blank" rel="noreferrer">Jose marcos Efigenio Da Silva</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
