1c561b914cef246c9fb4bfddc96c7ce1fbce7690
Jose marcos <72453138+SakakibaraKun@users.noreply.github.com>
2021-11-06 22:24:54 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -2,6 +2,7 @@ import Chart from 'react-apexcharts';
 import axios from 'axios';
 import { BASE_URL } from 'utils/requests';
 import { SaleSum } from 'types/sale';
+import { useEffect, useState } from 'react';
 
 type ChartData = {
     labels: string[];
@@ -11,21 +12,20 @@ type ChartData = {
 
 const DonutChart = () => {
 
-    //FORMA ERRADA
-    let ChartData: ChartData = { labels: [], series: [] };
 
+    const [chartData, setChartData] = useState<ChartData>({labels:[], series: []});
 
-    //FORMA ERRADA
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
         .then((response) => {
-
             const data = response.data as SaleSum[];
             const myLabels = data.map(x => x.sellerName);
             const mySeries = data.map(x => x.sum);
 
-            ChartData = {labels: myLabels, series: mySeries}
-            console.log(response.data);
+            setChartData({labels: myLabels, series: mySeries});
         });
+    }, []);
+
 
 
 
@@ -42,8 +42,8 @@ const DonutChart = () => {
 
     return (
         <Chart
-            options={{ ...options, labels: ChartData.labels }}
-            series={ChartData.series}
+            options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
             type="donut"
             height="240"
         />
